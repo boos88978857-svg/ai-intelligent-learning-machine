70f276bb31746d6c7c0d7584e194765b17958eda
boos88978857-svg <boos88978857@gmail.com>
2026-01-19 12:45:02 +0700

    学习竞技场页面

---
+++ app/arena/page.ts
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>学习竞技场</h1>
+      <p>这里将放对战/排名/任务等竞技功能入口。</p>
+      <a href="/">← 回首页</a>
+    </main>
+  );
+}
