d9ae40a70be6c75e0e1534a0ee74cc0ba4fbe59f
boos88978857-svg <boos88978857@gmail.com>
2026-01-19 11:59:09 +0700

    更新 page.tsx

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -2,10 +2,22 @@ import Link from "next/link";
 
 export default function PracticePage() {
   return (
-    <main style={{ padding: 24 }}>
+    <main>
       <h1>练习区</h1>
-      <p>如果你看到这页，代表 /practice 路由成功 ✅</p>
-      <Link href="/">回首页</Link>
+      <p>请选择你要练习的内容：</p>
+
+      <ul>
+        <li>
+          <Link href="/practice/basic">
+            基础练习
+          </Link>
+        </li>
+        <li>
+          <Link href="/practice/advanced">
+            进阶练习
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
